0e142266624d7bc96f2b9d5f3cea252422bf94cb
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-18 15:01:06 +0700

    Menambahkan image umy

--- app/src/main/java/com/example/pertemuanbasiccompos/MainActivity.kt
+++ app/src/main/java/com/example/pertemuanbasiccompos/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,10 +45,16 @@ fun BasicColum(modifier: Modifier = Modifier) {
             fontSize = 50.sp,
             fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.padding(10.dp))
-
         Text( text = "Ini Adalah Halaman Login",
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold)
+        Spacer(modifier = Modifier.padding(20.dp))
+        Image(painter = painterResource(id = R.drawable.umy),
+            contentDescription = "",)
+
+
+
+    }
 }
 
 @Composable
