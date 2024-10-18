34167b84344ef6e2be9c80648a1f994ee63930fa
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-18 14:30:14 +0700

    membuat fungsi basiccolum dengan elemen ui dalam bentuk colum

--- app/src/main/java/com/example/pertemuanbasiccompos/MainActivity.kt
+++ app/src/main/java/com/example/pertemuanbasiccompos/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pertemuanbasiccompos.ui.theme.PertemuanBasicComposTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +28,15 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+@Composable
+fun BasicColum(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
+            .padding(top = 50.dp)) {
+
     }
 }
 
